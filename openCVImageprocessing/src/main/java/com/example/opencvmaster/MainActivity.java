9159ddf2a25def12615bf0dc38af2615cb8c8dcb
hjeong <hjeong@postech.ac.kr>
2021-03-05 22:05:32 +0500

    update

---
+++ openCVImageprocessing/src/main/java/com/example/opencvmaster/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.opencvmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenCVLoader.initDebug();
+    }
+
+//    TODO
+    public void convertImage(View v){
+//        Toast.makeText(this, "sfsfs", Toast.LENGTH_LONG).show();
+        Mat img = null;
+        try {
+            img = Utils.loadResource(getApplicationContext(), R.drawable.lena);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
+
+        Mat img_result = img.clone();
+        Imgproc.Canny(img, img_result, 80, 90);
+        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(img_result, img_bitmap);
+        ImageView imageView = findViewById(R.id.id_image);
+        imageView.setImageBitmap(img_bitmap);
+    }
+
+}
