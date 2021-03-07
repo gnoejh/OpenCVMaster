154dbc5fb63f828ded3ea61f90f31dbb22e62671
hjeong <hjeong@postech.ac.kr>
2021-03-07 21:25:33 +0500

    update

--- openCVCamera/src/main/java/com/example/opencvproject/MainActivity.java
+++ openCVCamera/src/main/java/com/example/opencvproject/MainActivity.java
@@ -31,40 +31,8 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     private static String TAG = "MainActivity";
 
-    public static final int      VIEW_MODE_RGBA      = 0;
-    public static final int      VIEW_MODE_HIST      = 1;
-    public static final int      VIEW_MODE_CANNY     = 2;
-    public static final int      VIEW_MODE_SEPIA     = 3;
-    public static final int      VIEW_MODE_SOBEL     = 4;
-    public static final int      VIEW_MODE_ZOOM      = 5;
-    public static final int      VIEW_MODE_PIXELIZE  = 6;
-    public static final int      VIEW_MODE_POSTERIZE = 7;
-
-    private MenuItem mItemPreviewRGBA;
-    private MenuItem             mItemPreviewHist;
-    private MenuItem             mItemPreviewCanny;
-    private MenuItem             mItemPreviewSepia;
-    private MenuItem             mItemPreviewSobel;
-    private MenuItem             mItemPreviewZoom;
-    private MenuItem             mItemPreviewPixelize;
-    private MenuItem             mItemPreviewPosterize;
     private CameraBridgeViewBase mOpenCvCameraView;
-
-    private Size mSize0;
-
-    private Mat                  mIntermediateMat;
-    private Mat                  mMat0;
-    private MatOfInt mChannels[];
-    private MatOfInt             mHistSize;
-    private int                  mHistSizeNum = 25;
-    private MatOfFloat mRanges;
-    private Scalar mColorsRGB[];
-    private Scalar               mColorsHue[];
-    private Scalar               mWhilte;
-    private Point mP1;
-    private Point                mP2;
-    private float                mBuff[];
-    private Mat                  mSepiaKernel;
+    private Mat mIntermediateMat;
 
 //TODO activate opencv manager
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -127,21 +95,8 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat rgba = inputFrame.rgba();
-        Size sizeRgba = rgba.size();
+        mIntermediateMat = new Mat();
 
-        Mat rgbaInnerWindow;
-
-        int rows = (int) sizeRgba.height;
-        int cols = (int) sizeRgba.width;
-
-        int left = cols / 8;
-        int top = rows / 8;
-
-        int width = cols * 3 / 4;
-        int height = rows * 3 / 4;
-
-        mIntermediateMat = new Mat(); //TODO initialize
-        rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
         Imgproc.Canny(rgba, mIntermediateMat, 80, 90);
         Imgproc.cvtColor(mIntermediateMat, rgba, Imgproc.COLOR_GRAY2BGRA, 4);
 
