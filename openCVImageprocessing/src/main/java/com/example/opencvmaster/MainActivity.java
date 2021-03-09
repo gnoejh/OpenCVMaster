caf166d4ced6a2bc39fb855a4a9eceb092213377
hjeong <hjeong@postech.ac.kr>
2021-03-09 22:16:53 +0500

    update

--- openCVImageprocessing/src/main/java/com/example/opencvmaster/MainActivity.java
+++ openCVImageprocessing/src/main/java/com/example/opencvmaster/MainActivity.java
@@ -16,12 +16,14 @@ import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,14 +45,25 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
+//        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
 
-        Mat img_result = img.clone();
+        Mat ycrcb = null;
 
+        Imgproc.cvtColor(img,ycrcb,Imgproc.COLOR_RGB2YCrCb);
+Log.v(TAG,"saas");
+        List<Mat> channels = null;
+        Core.split(ycrcb,channels);
+
+        Imgproc.equalizeHist(channels.get(0), channels.get(0));
+
+        Mat result = null;
+        Core.merge(channels,ycrcb);
+
+        Imgproc.cvtColor(ycrcb,result,Imgproc.COLOR_YCrCb2BGR);
 
         // Output
-        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(),Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(img_result, img_bitmap);
+        Bitmap img_bitmap = Bitmap.createBitmap(result.cols(), result.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(result, img_bitmap);
         ImageView imageView = findViewById(R.id.id_image);
         imageView.setImageBitmap(img_bitmap);
     }
